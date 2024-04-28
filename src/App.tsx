98cd37464585fa4d112fd30e4871e5b314b6ad98
LaptopNickolas <panteleew.kolya@gmail.com>
2024-04-28 18:38:29 +0900

    поправил роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,9 @@ import React, {useEffect, useMemo} from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import './App.css';
-import Basket from './components/SavedMovies/SavedMovies';
 import { Routes, Route } from 'react-router-dom';
 import Main from './components/Main/Main';
-import Registration from './components/Registration/Registration';
-import Login from './components/Login/Login';
-import { Profile } from './components/Profile';
-import Catalog from './components/Catalog/Catalog';
-import { ProtectedRoute } from './components/ProtectedRoute';
-import { UseAuthorization } from './hooks/UseForm/UseAuthorization';
 import { MoviePage } from './components/MoviePage';
-import { AboutProject } from './components/AboutProject';
-import { getMovies } from './utils/moviesApi/moviesApi';
 import SavedMovies from './components/SavedMovies/SavedMovies';
 
 function App() {
@@ -24,7 +15,7 @@ function App() {
       <div className='content'>
         <Routes>
           <Route
-          path='/'
+          path='/vktest-movies/'
           element={<Main />}
           />
 
@@ -32,35 +23,6 @@ function App() {
           path='/favorites'
           element={<SavedMovies />}
           />
-
-          {/* <Route
-          path='/sign-up'
-          element={<Registration />}
-          />
-
-          <Route
-          path='/sign-in'
-          element={<Login />}
-          />
-
-          <Route
-          path='/catalog'
-          element={<Catalog />}
-          />
-
-          <Route 
-          path='/profile'
-          element={
-            <ProtectedRoute 
-            component={Profile}
-            />
-          }
-          />
-
-          <Route 
-          path='/about-project'
-          element={<AboutProject />}
-          /> */}
 
           <Route
           path='/movie/:id'
